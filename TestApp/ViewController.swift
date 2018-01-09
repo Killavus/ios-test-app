5c735c479b49cc833887a6ff3b65e2c571a05065
Marcin Grzywaczewski <killavus@gmail.com>
2018-01-09 10:38:31 +0100

    Initial commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Marcin Grzywaczewski on 09/01/2018.
+//  Copyright © 2018 Marcin Grzywaczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
